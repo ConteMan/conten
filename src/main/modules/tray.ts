47f3e54c3465460d47e815128746cd3309ccaa5f
ConteMan <real.conteman@gmail.com>
2022-05-27 08:52:38 +0800

    feat: 系统托盘图标点击隐藏或展示应用功能;

--- src/main/modules/tray.ts
+++ src/main/modules/tray.ts
@@ -4,4 +4,10 @@ import { Tray } from 'electron'
 export function trayInit() {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const tray = new Tray(path.join(__dirname, '../public/images/logo_16.png'))
+
+  tray.setToolTip('Contea Desktop')
+  tray.on('click', () => {
+    if (global.win)
+      global.win?.isVisible() ? global.win?.hide() : global.win?.show()
+  })
 }
