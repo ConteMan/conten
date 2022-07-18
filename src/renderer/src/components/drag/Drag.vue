c3e0108469725297ef16cfe9bd43b2b9aea8183a
ConteMan <real.conteman@gmail.com>
2022-07-18 17:55:20 +0800

    feat: 面板，修改为自由布局，可设置位置，调节大小;

---
+++ src/renderer/src/components/drag/Drag.vue
@@ -0,0 +1,120 @@
+<script setup lang="ts">
+import { useDraggable, useStorage, watchDebounced } from '@vueuse/core'
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  dWidth: {
+    type: Number,
+    default: 200,
+  },
+  dHeight: {
+    type: Number,
+    default: 100,
+  },
+})
+
+const storageValue = useStorage(`drag-${props?.name}`, {} as any)
+
+const data = reactive({
+  name: props.name,
+  x: storageValue.value?.x ?? 100,
+  y: storageValue.value?.y ?? 200,
+  width: storageValue.value?.width ?? props.dWidth,
+  height: storageValue.value?.height ?? props.dHeight,
+  clientXStart: 0,
+  clientYStart: 0,
+})
+
+const el = ref<HTMLElement | null>(null)
+const resizeEl = ref<HTMLElement | null>(null)
+
+const { x, y } = useDraggable(el, {
+  initialValue: { x: data.x, y: data.y },
+  preventDefault: true,
+})
+data.x = x
+data.y = y
+
+const dealStyle = computed(() => {
+  return {
+    left: `${data.x > 0 ? data.x : 0}px`,
+    top: `${data.y - 32 > 0 ? data.y - 32 : 0}px`,
+    width: `${data.width}px`,
+    height: `${data.height}px`,
+  }
+})
+
+const resize = (x: number, y: number) => {
+  const computedX = x - data.clientXStart
+  data.width += computedX
+  data.clientXStart = x
+
+  const computedY = y - data.clientYStart
+  data.height += computedY
+  data.clientYStart = y
+}
+
+onMounted(() => {
+  resizeEl.value?.addEventListener('mousedown', (e) => {
+    data.clientXStart = e.clientX
+    data.clientYStart = e.clientY
+    e.preventDefault()
+
+    document.onmousemove = (e) => {
+      resize(e.clientX, e.clientY)
+    }
+
+    document.onmouseup = (e) => {
+      document.onmouseup = null
+      document.onmousemove = null
+    }
+  })
+})
+
+const storageObj = computed(() => {
+  return {
+    x: data.x,
+    y: data.y,
+    width: data.width,
+    height: data.height,
+  }
+})
+storageValue.value = storageObj.value
+
+watchDebounced(storageObj,
+  () => {
+    storageValue.value = storageObj.value
+  }, { debounce: 500 })
+</script>
+
+<template>
+  <div
+    class="drag-container absolute overflow-auto z-auto hover:(shadow shadow-sm shadow-gray-400)"
+    :style="dealStyle"
+  >
+    <div
+      ref="el"
+      class="drag-bar h-[20px] w-full cursor-move"
+    />
+    <slot />
+    <div
+      ref="resizeEl"
+      class="resize-btn absolute flex bottom-0 right-0 cursor-pointer opacity-0"
+    >
+      <mdi-arrow-bottom-right-thick />
+    </div>
+  </div>
+</template>
+
+<style lang="less">
+.drag-container {
+  &:hover {
+    .resize-btn{
+      opacity: 100;
+    }
+  }
+}
+</style>
--- src/renderer/src/views/dashboard/Dashboard.vue
+++ src/renderer/src/views/dashboard/Dashboard.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import _ from 'lodash'
+import Drag from '@renderer/components/drag/Drag.vue'
 
 const cards = [
   'now',
@@ -10,66 +10,28 @@ const cards = [
 ]
 const data = reactive({
   showCards: cards as string[],
-  showSetting: false,
 })
-const { showCards, showSetting } = toRefs(data)
-
-const showDeal = (module: string) => {
-  if (data.showCards.includes(module)) {
-    _.remove(data.showCards, (item) => {
-      return module === item
-    })
-  }
-  else {
-    data.showCards.push(module)
-  }
-  // eslint-disable-next-line no-console
-  console.log(data.showCards)
-}
+const { showCards } = toRefs(data)
 </script>
 
 <template>
-  <div class="flex flex-row items-start">
-    <div class="pl-4 flex-grow gap-2">
-      <NowCard v-if="showCards.includes('now')" class="w-full flex-shrink-0" />
-      <WeatherCard v-if="showCards.includes('weather')" />
-      <WakaTimeCard v-if="showCards.includes('wakatime')" />
-      <TapTapCard v-if="showCards.includes('taptap')" />
-      <V2exCard v-if="showCards.includes('v2ex')" />
-      <SubjectCard v-if="showCards.includes('subject')" />
-    </div>
-    <div
-      class="setting-action flex-shrink-0 w-[10px] h-full flex justify-center items-center cursor-pointer hover:(bg-light-400)"
-      @click="() => showSetting = !showSetting"
-    >
-      <div class="setting-icon h-0 w-0" />
-    </div>
-    <div v-if="showSetting" class="setting-container bg-light-400 w-[300px] h-full flex-shrink-0 p-4">
-      <div
-        class="bg-light-800 flex justify-center items-center mb-4 py-1 text-[14px] font-bold hover:(text-red-400) cursor-pointer"
-        @click="() => showSetting = !showSetting"
-      >
-        x
-      </div>
-      <div class="mb-2">
-        模块
-      </div>
-      <div class="flex flex-col gap-1">
-        <div
-          class="px-2 bg-blue-200 cursor-pointer"
-          :class="{ '!bg-red-400': showCards.includes('now') }"
-          @click="showDeal('now')"
-        >
-          时间
-        </div>
-        <div
-          class="px-2 bg-blue-200 cursor-pointer"
-          :class="{ '!bg-red-400': showCards.includes('weather') }"
-          @click="showDeal('weather')"
-        >
-          天气
-        </div>
-      </div>
+  <div>
+    <div class="w-full h-full relative">
+      <drag v-if="showCards.includes('v2ex')" name="v2ex">
+        <V2exCard />
+      </drag>
+      <drag v-if="showCards.includes('taptap')" name="taptap">
+        <TapTapCard />
+      </drag>
+      <drag v-if="showCards.includes('wakatime')" name="wakatime">
+        <WakaTimeCard />
+      </drag>
+      <drag v-if="showCards.includes('weather')" name="weather">
+        <WeatherCard />
+      </drag>
+      <drag v-if="showCards.includes('now')" name="now">
+        <NowCard class="w-full flex-shrink-0 text-red-400 font-bold text-[14px]" />
+      </drag>
     </div>
   </div>
 </template>
@@ -77,5 +39,11 @@ const showDeal = (module: string) => {
 <style lang="less">
 .setting-action {
   border-radius: 4px 0 0 4px;
+}
+.vue-resizable-handle {
+  visibility: hidden;
+}
+.vue-grid-item:hover > .vue-resizable-handle {
+  visibility: visible;
 }
 </style>
